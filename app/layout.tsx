1696d0270853db89991cdf5b3b8174fb651cae01
Asraful-code235 <asrafulislamcodes@gmail.com>
2023-06-24 13:08:06 +0600

    cobra

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,6 @@ import { cn } from "@/lib/utils"
 import FooterPage from "@/components/Footer-component"
 import { SiteHeader } from "@/components/site-header"
 
-export const metadata: Metadata = {
-  title: "CobraSito - Sito di Casino",
-  description:
-    "Benvenuti al sito di casino CobraSito, dove puoi trovare una vasta selezione di slot machine e tanto divertimento. Scopri le nostre nuove slot, le ultime notizie del settore e tanto altro.",
-}
-
 interface RootLayoutProps {
   children: React.ReactNode
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import { Metadata } from "next"
 import Image from "next/image"
+import { Helmet } from "react-helmet"
 
 import Guide from "@/components/slot/Guide"
 import NewSlotMachines from "@/components/slot/NewSlotMachines"
@@ -28,37 +29,46 @@ export default function IndexPage() {
   }
 
   return (
-    <section className="-mt-1 pb-24">
-      <div className="w-screen bg-white">
-        <Slider {...settings}>
-          <div className="h-[80vh] w-screen bg-red-500">
-            <Image
-              alt="slider__image"
-              src={"/images/image1.jpg"}
-              className="h-full w-full object-cover object-center"
-              width={1000}
-              height={500}
-            />
-          </div>
-          <div className="h-[80vh] w-screen bg-red-500">
-            <Image
-              alt="slider__image"
-              src={"/images/image2.jpg"}
-              className="h-full w-full object-cover object-center"
-              width={1000}
-              height={500}
-            />
-          </div>
-        </Slider>
-      </div>
-      <section className="bg-white py-16">
-        <NewSlotMachines />
+    <>
+      <Helmet>
+        <title>Cobra Sito</title>
+        <meta
+          name="description"
+          content="Esplora una vasta selezione di slot online su cobra sito. Goditi l'emozione dei giochi di slot con grafica coinvolgente, funzioni speciali e la possibilità di vincere premi fantastici. Registrati ora per un'esperienza di gioco indimenticabile!"
+        />
+      </Helmet>
+      <section className="-mt-1 pb-24">
+        <div className="w-screen bg-white">
+          <Slider {...settings}>
+            <div className="h-[80vh] w-screen bg-red-500">
+              <Image
+                alt="slider__image"
+                src={"/images/image1.jpg"}
+                className="h-full w-full object-cover object-center"
+                width={1000}
+                height={500}
+              />
+            </div>
+            <div className="h-[80vh] w-screen bg-red-500">
+              <Image
+                alt="slider__image"
+                src={"/images/image2.jpg"}
+                className="h-full w-full object-cover object-center"
+                width={1000}
+                height={500}
+              />
+            </div>
+          </Slider>
+        </div>
+        <section className="bg-white py-16">
+          <NewSlotMachines />
+        </section>
+        <News />
+        <Poster />
+        <ThemeSection />
+        <RedPoster />
+        <Guide />
       </section>
-      <News />
-      <Poster />
-      <ThemeSection />
-      <RedPoster />
-      <Guide />
-    </section>
+    </>
   )
 }
