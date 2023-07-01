74a23d16e4a76fb363ae773de3a1a8448311f585
Asraful-code235 <asrafulislamcodes@gmail.com>
2023-07-02 01:57:14 +0600

    fixed image for the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
 import Head from "next/head"
+import { MantineProvider } from "@mantine/core"
+import { createGetInitialProps } from "@mantine/next"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import { fontSans } from "@/lib/fonts"
@@ -14,25 +16,28 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
+const getInitialProps = createGetInitialProps()
 const queryClient = new QueryClient()
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
         <QueryClientProvider client={queryClient}>
-          <Head>Slot</Head>
-          <body
-            className={cn(
-              "min-h-screen overflow-x-hidden bg-background font-sans antialiased",
-              fontSans.variable
-            )}
-          >
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <main className="flex-1">{children}</main>
-              <FooterPage />
-            </div>
-          </body>
+          <MantineProvider>
+            <Head>Slot</Head>
+            <body
+              className={cn(
+                "min-h-screen overflow-x-hidden bg-background font-sans antialiased",
+                fontSans.variable
+              )}
+            >
+              <div className="relative flex min-h-screen flex-col">
+                <SiteHeader />
+                <main className="flex-1">{children}</main>
+                <FooterPage />
+              </div>
+            </body>
+          </MantineProvider>
         </QueryClientProvider>
       </html>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import Slider, { Settings } from "react-slick"
-
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import { useRef } from "react"
 import { Metadata } from "next"
 import Image from "next/image"
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline"
+import { Carousel } from "@mantine/carousel"
+import { rem } from "@mantine/core"
+import Autoplay from "embla-carousel-autoplay"
 import { Helmet } from "react-helmet"
 
 import Guide from "@/components/slot/Guide"
@@ -15,19 +16,17 @@ import Poster from "@/components/slot/Poster"
 import RedPoster from "@/components/slot/RedPoster"
 import ThemeSection from "@/components/slot/ThemeSection"
 
+const items = [
+  {
+    img: "/images/image2.jpg",
+  },
+  {
+    img: "/images/image1.jpg",
+  },
+]
+
 export default function IndexPage() {
-  const settings: Settings = {
-    dots: true,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 7000,
-    cssEase: "linear",
-    arrows: true,
-  }
+  const autoplay = useRef(Autoplay({ delay: 7000 }))
 
   return (
     <>
@@ -38,30 +37,57 @@ export default function IndexPage() {
           content="Esplora una vasta selezione di slot online su cobra sito. Goditi l'emozione dei giochi di slot con grafica coinvolgente, funzioni speciali e la possibilità di vincere premi fantastici. Registrati ora per un'esperienza di gioco indimenticabile!"
         />
       </Helmet>
-      <section className="-mt-1 pb-24">
-        <div className="w-screen bg-white">
-          <Slider {...settings}>
-            <div className="h-[80vh] w-screen bg-red-500">
+      <section className="-mt-1 ">
+        <div className=" ">
+          <Carousel
+            // maw={320}
+            mx="auto"
+            loop
+            dragFree
+            withIndicators
+            // height={200}
+            speed={5000}
+            plugins={[autoplay.current]}
+
+            // onMouseEnter={autoplay.current.stop}
+            // onMouseLeave={autoplay.current.reset}
+          >
+            {items.map((item) => (
+              <Carousel.Slide key={item.img}>
+                <Image
+                  alt="slider__image"
+                  src={item.img}
+                  className="aspect-video w-full object-cover object-center sm:aspect-[16/5] "
+                  width={1000}
+                  height={1000}
+                  priority
+                />
+              </Carousel.Slide>
+            ))}
+          </Carousel>
+
+          {/* <Slider {...settings}>
+            <div className="  ">
               <Image
                 alt="slider__image"
                 src={"/images/image1.jpg"}
-                className="h-full w-full object-cover object-center"
+                className="w-full aspect-[16/5] object-cover object-center"
                 width={600}
                 height={500}
                 priority
               />
             </div>
-            <div className="h-[80vh] w-screen bg-red-500">
+            <div className=" ">
               <Image
                 alt="slider__image"
                 src={"/images/image2.jpg"}
-                className="h-full w-full object-cover object-center"
+                className="w-full aspect-[16/5] object-cover object-center"
                 width={1000}
                 height={500}
                 priority
               />
             </div>
-          </Slider>
+          </Slider> */}
         </div>
         <section className="bg-white py-16">
           <NewSlotMachines />
